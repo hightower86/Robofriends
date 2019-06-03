b4c3b862e40488932c986767e1eac3276f7b2b2c
Adel <htwr86@gmail.com>
2019-06-03 10:18:00 +0300

    lesson 191

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
+ 
   return (
-  <div className='bg-light-green dib pa3 ma2 grow'>
-    <img src="robots" alt=""/>
-    <div>
-      <h2>Jane Doe</h2>
-      <img src="https://robohash.org/test?200x200" alt=""/>
-      <p>jane.doe@gmail.com</p>
+    <div className='tc bg-green dib br3 pa3 ma3 grow bw2 shadow-3'>
+      <img src="robots" alt=""/>
+      <div>
+        <h2>{name}</h2>
+        <img src={`https://robohash.org/${id}?200x200`} alt=""/>
+        <p>{email}</p>
+      </div>
     </div>
-  </div>)
+  );
 }
 
 export default Card;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots.js';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(
+ReactDOM.render( 
     <div>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+      <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
     </div>
     , document.getElementById('root'));
 
