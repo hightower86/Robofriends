a42695497f1b71558c4f553214c64b447c8b23b1
Adel <htwr86@gmail.com>
2019-06-04 08:30:08 +0300

    add searchbox

---
+++ src/SearchBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { declareVariable } from '@babel/types';
+
+const SearchBox = ({ searchField, searchChange}) => {
+  return (
+    <div className='pa2'>
+      <input 
+        className='pa3 ba b--green bg-lightest-blue'
+        type="search" 
+        placeholder="search Robots"
+        onChange={searchChange}
+      />
+    </div>
+    
+  );
+}
+
+export default SearchBox;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import Card from './Card';
-import Cardlist from './CardList';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots.js';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render( 
-    <Cardlist robots={robots}/>
-    , document.getElementById('root'));
+
+//ReactDOM.render(, document.getElementById('root'));
+ReactDOM.render( <App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
