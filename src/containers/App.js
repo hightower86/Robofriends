2a83e4d99bd92447469fffacf20d76402efeda1c
Adel <htwr86@gmail.com>
2019-07-20 20:29:12 +0300

    208.99 redux connect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox'
-import Scroll from '../components/Scroll'
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import { setSearchField } from '../actions';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: [],
-      searchField: ''
+      robots: []
     };
   }
 
@@ -19,12 +20,9 @@ class App extends Component {
       .then(users => this.setState({robots: users}));
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value })
-  }
-
   render() {
-    const { robots, searchField} = this.state;
+    const { robots} = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
     return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
@@ -33,7 +31,7 @@ class App extends Component {
       (
         <div className="tc">
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={ this.onSearchChange }/>
+          <SearchBox searchChange={ onSearchChange }/>
           <Scroll>
             <CardList robots={filteredRobots}/>
           </Scroll>
@@ -43,4 +41,17 @@ class App extends Component {
   } 
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    searchField: state.searchField
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -6,18 +6,12 @@ import { searchRobots } from './reducers';
 import './index.css';
 // import Card from './Card';
 import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
 const store = createStore(searchRobots);
-//ReactDOM.render(, document.getElementById('root'));
+
 ReactDOM.render( 
                 <Provider store={store}>
                   <App />
                 </Provider>
                 , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
