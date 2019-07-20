587ccf46c01475162d630008629ca8157c22fe7b
Adel <htwr86@gmail.com>
2019-07-20 12:55:57 +0300

    207.99 Redux Store and Provider

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,7 +11,6 @@ class App extends Component {
       robots: [],
       searchField: ''
     };
-    console.log('constructor');
   }
 
   componentDidMount() {
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { searchRobots } from './reducers';
 import './index.css';
 // import Card from './Card';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-
+const store = createStore(searchRobots);
 //ReactDOM.render(, document.getElementById('root'));
-ReactDOM.render( <App />, document.getElementById('root'));
+ReactDOM.render( 
+                <Provider store={store}>
+                  <App />
+                </Provider>
+                , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
